1b173d33c7bb0e51f794259be48837bf9246efa5
aharri64 <haveacompleteday@gmail.com>
2021-01-26 12:20:05 -0700

    built weapons console

--- js/spacemane.js
+++ js/spacemane.js
@@ -38,4 +38,22 @@ let shots = [];
 let targetingStatus = null;
 
 
-const randomWord = () => { answer = wordsToGuess[Math.floor(Math.random() * wordsToGuess)] };
+const randomWord = () => { answer = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)] };
+
+
+
+function buildConsole() {
+    let weaponControls = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        ` 
+        <button 
+        id='` + letter + `' 
+        onClick="takeAShot('` + letter + `')"
+        > 
+        ` + letter + `
+        </button>
+        `).join('');
+    document.getElementById("controls").innerHTML = weaponControls;
+}
+
+randomWord();
+buildConsole();
